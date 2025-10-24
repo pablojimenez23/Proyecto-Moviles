cbcaba543f4c96134e266f3d44d2aa3161c1fa67
Pablo <pp-pjimenez@zonar.cl>
2025-10-23 21:12:18 -0300

    Se cambio la version del emulador y se añadio un icono

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.proyectodd"
-        minSdk = 24
+        minSdk = 33        // ✅ CAMBIADO A TIRAMISU (Android 13)
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
